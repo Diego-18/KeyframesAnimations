838aa2e799a7d08da6ec7e51cf524da18860f2cf
Diego Chavez <ingdiegochavez18@gmail.com>
2021-08-11 04:57:25 -0400

    core/version-1.0
    ** Improved responsive design using flexbox
    
    ** Improved color material design
    
    ** Improved dynamic JavaScript code

---
+++ main.js
@@ -0,0 +1,84 @@
+function fjOption(vpOption){
+    let Option = vpOption;
+    let Item = document.getElementById('item__demo');
+
+    if(Option === 'Flash'){
+        Item.className = 'animacion1';
+    }
+
+    if(Option === 'Rebote'){
+        Item.className = 'animacion2';
+    }
+
+    if(Option === 'Chicle'){
+        Item.className = 'animacion3';
+    }
+
+    if(Option === 'GiroX'){
+        Item.className = 'animacion4';
+    }
+
+    if(Option === 'Giro3D'){
+        Item.className = 'animacion5';
+    }
+
+    if(Option === 'Salida'){
+        Item.className = 'animacion6';
+    }
+
+    if(Option === 'VibracionV'){
+        Item.className = 'animacion7';
+    }
+
+    if(Option === 'VibracionH'){
+        Item.className = 'animacion8';
+    }
+
+    if(Option === 'Cartel'){
+        Item.className = 'animacion9';
+    }
+
+    if(Option === 'Balanceo'){
+        Item.className = 'animacion10';
+    }
+
+    if(Option === 'Vaiven'){
+        Item.className = 'animacion11';
+    }
+
+    if(Option === 'ReboteIn'){
+        Item.className = 'animacion12';
+    }
+
+    if(Option === 'Click'){
+        Item.className = 'animacion13';
+    }
+
+    if(Option === 'Giro'){
+        Item.className = 'animacion14';
+    }
+
+    if(Option === 'RotativoIn'){
+        Item.className = 'animacion15';
+    }
+
+    if(Option === 'VelozIn'){
+        Item.className = 'animacion16';
+    }
+
+    if(Option === 'VelozOut'){
+        Item.className = 'animacion17';
+    }
+
+    if(Option === 'Descolgado'){
+        Item.className = 'animacion18';
+    }
+
+    if(Option === 'Aparicion'){
+        Item.className = 'animacion19';
+    }
+
+    if(Option === 'RotativoOut'){
+        Item.className = 'animacion20';
+    }
+}
